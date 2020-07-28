a310a37796e97706f58c849facc988caeea031c3
Taylor Southwick <twsouthwick@outlook.com>
2020-07-28 12:25:51 -0700

    Add repro

---
+++ src/Repro/Program.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using MvvmBlazor.Extensions;
+
+namespace Repro
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            builder.Services.AddMvvm();
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
---
+++ src/Repro/ReproComponent.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components.Rendering;
+using Microsoft.Extensions.Configuration;
+using MvvmBlazor.Components;
+using MvvmBlazor.ViewModel;
+using System.Collections.ObjectModel;
+
+namespace Repro
+{
+    public class ReproComponent : MvvmComponentBase
+    {
+        private readonly TestModel Model = new TestModel();
+
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            Bind(Model, t => t.Counts);
+
+            Model.Counts.Add(1);
+
+            base.BuildRenderTree(builder);
+        }
+
+        private class TestModel : ViewModelBase
+        {
+            public ObservableCollection<int> Counts { get; } = new ObservableCollection<int>();
+        }
+    }
+}
